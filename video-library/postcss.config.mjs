490ba870dd31365765e59fd15566a4c012da80ef
Tomáš Endrych <tomas.endrych@superkoders.com>
2024-04-25 18:21:39 +0200

    Refactor PostCSS and TailwindCSS configurations

--- video-library/postcss.config.mjs
+++ video-library/postcss.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    'tailwindcss/nesting': {},
+	plugins: {
+		'tailwindcss/nesting': {},
 		tailwindcss: {},
-		autoprefixer: {},
-  },
+		autoprefixer: {}
+	}
 };
 
 export default config;
