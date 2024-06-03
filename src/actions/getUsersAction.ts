6cfaf03dbb6a49c2c7869a6a0bdfbf9c76c822e0
Patrik Ludvik <patrik.ludvik@itixo.com>
2024-06-03 06:25:23 +0200

    try catch pryc

--- src/actions/getUsersAction.ts
+++ src/actions/getUsersAction.ts
@@ -5,14 +5,9 @@ import { UserController } from '../database/controllers';
 import { usersListLength } from '../util/constants';
 
 export const getUserAction = async () => {
-	try {
-		const db = await withDatabase();
-		const userController = new UserController(db);
-		const users = await userController.list(1, usersListLength);
+	const db = await withDatabase();
+	const userController = new UserController(db);
+	const users = await userController.list(1, usersListLength);
 
-		return users.map((user) => ({ ...user, role: { ...user.role } }));
-	} catch (error) {
-		console.error('Error fetching users:', error);
-		throw error;
-	}
+	return users.map((user) => ({ ...user, role: { ...user.role } }));
 };
