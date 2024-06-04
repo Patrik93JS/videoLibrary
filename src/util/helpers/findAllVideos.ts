ddccb576d465100b7fd8f679a93a57bf29f2267c
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2024-06-04 16:54:40 +0200

    mb join

--- src/util/helpers/findAllVideos.ts
+++ src/util/helpers/findAllVideos.ts
@@ -5,5 +5,5 @@ export const findAllVideos = async () => {
 	const db = await withDatabase();
 	const videoController = new VideoController(db);
 
-	const videoData = await videoController.list(1);
+	const videoData = (await videoController.list(1)).join('files');
 };
